415e30e244f9235908d1ec10db12996f3e77d735
jwright159 <iamjwright159@gmail.com>
2019-03-16 15:56:51 -0400

    Added skipping spelled out dialogs

--- gdx-game/src/wrightway/gdx/tnb/Action.java
+++ gdx-game/src/wrightway/gdx/tnb/Action.java
@@ -50,14 +50,20 @@ public abstract class Action implements Runnable{
 		@Override
 		public boolean stop(boolean touched){
 			if((chara.delay == 0 && (!tap || (tap && touched))) || (chara.delay > 0 && tapDelay && touched)){
-				if(tapDelay)
-					while(chara.actions.size > 0 && chara.actions.get(0) instanceof DialogAction && ((DialogAction)chara.actions.get(0)).tapDelay)
+				if(tapDelay && touched){
+					//Log.debug(chara.actions);
+					while(!chara.actions.isEmpty() && chara.actions.get(0) instanceof DialogAction && ((DialogAction)chara.actions.get(0)).tapDelay)
 						chara.actions.removeIndex(0);
+				}
 				Tenebrae.player.dialogBox.setText(null);
 				Log.verbose2("Ending Dialog!", chara.delay, tap, touched, tapDelay);
 				return true;
 			}
 			return false;
+		}
+		@Override
+		public String toString(){
+			return super.toString()+"§"+tap+","+tapDelay+","+text;
 		}
 	}
 	
